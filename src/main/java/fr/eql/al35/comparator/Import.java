c11ae8e9e1cebf78adb951ebe26ef0f1b64b7744
Romain B <r.bertoux@free.fr>
2021-08-07 15:18:48 +0200

    +refacto de la méthode de récupération/copie en local du fichier source

--- src/main/java/fr/eql/al35/comparator/Import.java
+++ src/main/java/fr/eql/al35/comparator/Import.java
@@ -5,6 +5,7 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
@@ -46,33 +47,38 @@ public class Import implements CommandLineRunner {
 		merchantList.add(rakuten);
 		merchantList.add(alternate);
 		for (Merchant merchant : merchantList) {
+			
 			System.out.println("꧁∙∙∙∙∙·▫▫ᵒᴼᵒ▫ₒₒ▫ᵒᴼᵒ▫ₒₒ▫ Récupération du fichier ▫ₒₒ▫ᵒᴼᵒ▫ₒₒ▫ᵒᴼᵒ▫▫·∙∙∙∙∙꧂");
+			fetchFile(merchant);
 
-
-			FileUtils.copyURLToFile(
-					new URL(merchant.getUrlSource()),
-					new File("./src/main/resources/flux/" + merchant.getMerchantName() + "." + merchant.getFileType()));
-			
-			System.out.println("꧁∙∙∙∙∙·▫▫ᵒᴼᵒ▫ₒₒ▫ᵒᴼᵒ▫ₒₒ▫ Fichier récupéré▫ₒₒ▫ᵒᴼᵒ▫ₒₒ▫ᵒᴼᵒ▫▫·∙∙∙∙∙꧂");
-			if (merchant.getFileType() == "gzip") {
-			unGunzipFile("./src/main/resources/flux/" + merchant.getMerchantName().toLowerCase() + "." + merchant.getFileType(), "./src/main/resources/flux/" + merchant.getMerchantName().toLowerCase() + ".csv"); //décompression
-			}
-
-			
 			System.out.println("꧁∙∙∙∙∙·▫▫ᵒᴼᵒ▫ₒₒ▫ᵒᴼᵒ▫ₒₒ▫ Début de l'import des produits dans la base ▫ₒₒ▫ᵒᴼᵒ▫ₒₒ▫ᵒᴼᵒ▫▫·∙∙∙∙∙꧂");
 			FPS.csvToOffer(convertCSVtoInputStream("./src/main/resources/flux/" + merchant.getMerchantName().toLowerCase() + ".csv"), merchant.getId());
+			
 			System.out.println("꧁∙∙∙∙∙·▫▫ᵒᴼᵒ▫ₒₒ▫ᵒᴼᵒ▫ₒₒ▫ Fin de l'import des produits dans la base ▫ₒₒ▫ᵒᴼᵒ▫ₒₒ▫ᵒᴼᵒ▫▫·∙∙∙∙∙꧂");
 		}
 
 	}
 
-
+	//conversion du fichier en InputStream nécessaire au CSVParser
 	public static InputStream convertCSVtoInputStream(String path) throws IOException {
 		File initialFile = new File(path);
 		InputStream targetStream = new FileInputStream(initialFile); //conversion du fichier en InputStream
 		return targetStream;
 	}
 
+	//récupération et copier du fichier dans un répertoire donné. 
+	public void fetchFile (Merchant merchant) throws MalformedURLException, IOException {
+		FileUtils.copyURLToFile(
+				new URL(merchant.getUrlSource()),
+				new File("./src/main/resources/flux/" + merchant.getMerchantName() + "." + merchant.getFileType()));
+		
+		System.out.println("꧁∙∙∙∙∙·▫▫ᵒᴼᵒ▫ₒₒ▫ᵒᴼᵒ▫ₒₒ▫ Fichier récupéré▫ₒₒ▫ᵒᴼᵒ▫ₒₒ▫ᵒᴼᵒ▫▫·∙∙∙∙∙꧂");
+		if (merchant.getFileType() == "gzip") {
+		unGunzipFile("./src/main/resources/flux/" + merchant.getMerchantName().toLowerCase() + "." + merchant.getFileType(), "./src/main/resources/flux/" + merchant.getMerchantName().toLowerCase() + ".csv"); //décompression
+		}
+	}
+	
+	//décompression du fichier
 	public void unGunzipFile(String compressedFile, String decompressedFile) {
 
 		byte[] buffer = new byte[1024];
